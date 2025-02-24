89801eadc2c4f8dee0b27300ee5d185943ad9e4a
SrArtur2000 <srartur2000@usp.br>
2025-02-24 19:46:11 -0300

    Código ajustado e com os observáveis corretos. Falta calcular mais observáveis, em especial cumulante de binder. Próximos passos é isso e finite size scaling

--- algwolff.f90
+++ algwolff.f90
@@ -5,15 +5,15 @@ program wolff
   integer, parameter :: iseed = 10
   integer, parameter :: Nth = 3000, N = L**2
 
-  integer, dimension(2,L,L) :: S
+  integer, dimension(L,L) :: S
   integer, dimension(4,L) :: nn
-  integer :: i,j,k, ix, iy,i5,j5,j6
+  integer :: i,j,k, ix, iy,i5,j5,j6,contador,csize,csizem
   real(8) :: Em,Mm,beta,T,E1,M1
   real :: r
 
   open(40,file='MxT.dat')
   open(50,file='ExT.dat')
-
+  open(60,file='ClxT.dat')
   call random_init(.true.,.true.)
 
   do i = 1, L 
@@ -23,10 +23,13 @@ program wolff
     nn(4,i) = L - mod(L-i+1,L)
   enddo
   
-  call startlattice(S,3)
+  call startlattice(S,1)
 
   T = 0.1d0
+  contador = 1
   do while(T .le. 5.0d0)
+    contador = contador + 1
+    csizem = 0
     print*, "Estamos na Temperatura", T
     beta = 1.0d0/T
     Em = 0.0d0
@@ -38,13 +41,16 @@ program wolff
           ix = L*r + 1
           call random_number(r)
           iy = L*r + 1
-          call wolffmc(S,ix,iy,nn)
+          call wolffmc(S,ix,iy,nn,csize)
+          csizem = csizem + csize
         enddo
       enddo
+      csizem = csizem/N
       call measure(S,E1,M1,i5,nn)
       Em = Em + E1
       Mm = Mm + M1
     enddo
+    write(60,*)T,csizem
     write(40,*)T,Mm/Nth
     write(50,*)T,Em/Nth
     T = T + 0.01d0
@@ -53,21 +59,19 @@ contains
 
 subroutine startlattice(S,istart)
   implicit none
-  integer, dimension(2,L,L) :: S
+  integer, dimension(L,L) :: S
   integer :: istart, ir
 
   if(istart .eq. 1) then
     do i = 1,L
       do j = 1,L
-        S(1,i,j) = 1
-        S(2,i,j) = 0
+        S(i,j) = 1
       end do
     enddo
     else if(istart .eq. 2) then
      do i = 1,L
       do j = 1,L
-        S(1,i,j) = -1
-        S(2,i,j) = 0
+        S(i,j) = -1
       end do
     enddo
   else
@@ -75,17 +79,16 @@ subroutine startlattice(S,istart)
       do j = 1,L
         call random_number(r)
         ir = 2*r
-        S(1,i,j) = 1 - 2*ir
-        S(2,i,j) = 0
+        S(i,j) = 1 - 2*ir
       end do
     enddo
   endif
 end subroutine startlattice
 
-subroutine wolffmc(S,ix,iy,nn)
+subroutine wolffmc(S,ix,iy,nn,icont)
   implicit none
 
-  integer, dimension(2,L,L) :: S
+  integer, dimension(L,L) :: S
   integer, dimension(2,N) :: c
   integer :: icluster, ix, iy, icont, ica, dir
   integer, dimension(4,L) :: nn
@@ -99,24 +102,24 @@ subroutine wolffmc(S,ix,iy,nn)
   icluster = 0
   c(1,ica) = ix
   c(2,ica) = iy
-  S(2,c(1,ica),c(2,ica)) = 1
+  S(c(1,ica),c(2,ica)) = -S(c(1,ica),c(2,ica))
   ipos = (/c(1,ica),c(2,ica)/)
   do i = 1, 4 
     dir = mod(i,2) + (1+(-1)**i)
     ipv = ipos
     ipv(dir) = nn(i,ipv(dir))
     call random_number(r1)
-!   print*, 1-exp(dmin1(0.0d0,-2.0d0*beta*S(1,ipos(1),ipos(2))*S(1,ipv(1),ipv(2))))
-    if(r1 .le. (1 - exp(dmin1(0.0d0,-2.0d0*beta*S(1,ipos(1),ipos(2))*S(1,ipv(1),ipv(2))))) ) then
+!   print*, 1-exp(dmin1(0.0d0,2.0d0*beta*S(ipos(1),ipos(2))*S(ipv(1),ipv(2))))
+    if(r1 .le. (1 - exp(dmin1(0.0d0,2.0d0*beta*S(ipos(1),ipos(2))*S(ipv(1),ipv(2))))) ) then
       icont = icont + 1
       c(1,icont) = ipv(1)
       c(2,icont) = ipv(2)
-      S(2,c(1,icont),c(2,icont)) = 1
+      S(ipv(1),ipv(2)) = -S(ipv(1),ipv(2))
       icluster = 1
     endif
   enddo
   
-  do while(icluster .eq. 1)
+  do while(ica .lt. icont)
     ica = ica + 1
     icluster = 0
     ipos = (/c(1,ica),c(2,ica)/)
@@ -124,30 +127,23 @@ subroutine wolffmc(S,ix,iy,nn)
       dir = mod(i,2) + (1+(-1)**i)
       ipv = ipos
       ipv(dir) = nn(i,ipv(dir))
-      if(S(2,ipv(1),ipv(2)) .eq. 0) then
-        call random_number(r1)
-        if(r1 .le. (1 - exp(dmin1(0.0d0,-2.0d0*beta*S(1,ipos(1),ipos(2))*S(1,ipv(1),ipv(2))))) ) then
-          icont = icont + 1
-          c(1,icont) = ipv(1)
-          c(2,icont) = ipv(2)
-          S(2,c(1,icont),c(2,icont)) = 1
-          icluster = 1
-        endif
+      call random_number(r1)
+      if(r1 .le. (1 - exp(dmin1(0.0d0,2.0d0*beta*S(ipos(1),ipos(2))*S(ipv(1),ipv(2))))) ) then
+        icont = icont + 1
+        c(1,icont) = ipv(1)
+        c(2,icont) = ipv(2)
+        S(ipv(1),ipv(2)) = -S(ipv(1),ipv(2))
+        icluster = 1
       endif
     enddo
   enddo
-
 ! print*, "Esse cluster teve", icont, "Spins"
-  do i = 1, icont
-    S(1,c(1,i),c(2,i)) = -S(1,c(1,i),c(2,i))
-    S(2,c(1,i),c(2,i)) = 0
-  enddo
 end subroutine wolffmc
 
 subroutine measure(S,E,M,i,nn)
   implicit none
 
-  integer, dimension(2,L,L) :: S
+  integer, dimension(L,L) :: S
   real(8) :: E,M,Em,Mm
   integer :: i, i1
   integer, dimension(4,L) :: nn
@@ -156,15 +152,17 @@ subroutine measure(S,E,M,i,nn)
   M = 0.0d0
   do i1 = 1,L
     do j = 1,L
-      M = M + S(1,i1,j)
-      E = E - S(1,i1,j)*(S(1,i1,nn(2,j)) + S(1,i1,nn(4,j))+ S(1,nn(1,i1),j)+S(1,nn(3,i1),j) ) 
+      M = M + S(i1,j)
+      E = E - S(i1,j)*(S(i1,nn(2,j)) + S(i1,nn(4,j))+ S(nn(1,i1),j)+S(nn(3,i1),j) ) 
     enddo
   enddo
-  M = M/N
+  M = abs(M)/N
   E = E/N
   Em = E
-  Mm = M
-! write(10,*)i, M
+  Mm = abs(M)
+  if(T .eq. 0.1d0) then  
+    write(10,*)i, M
+  endif
 ! write(20,*)i, E
 end subroutine measure
 
